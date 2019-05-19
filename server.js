84901c16ece8608133054b1036da27599e1cd507
aburgan <44406493+4ortytwo@users.noreply.github.com>
2019-05-19 18:50:18 +0200

    user login

--- server.js
+++ server.js
@@ -6,19 +6,27 @@ const port = process.env.PORT || 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-
 app.prepare().then(() => {
   const server = express();
 
-  server.listen(port, err => {
-    if (err) throw err;
+  server.use(express.json());
 
-    console.log(`Listening on port ${port}`);
+  server.post("/api/login", (req, res) => {
+    const { email, password } = req.body;
+    res.json({
+      email,
+      password,
+      success: true
+    });
   });
 
   server.get("*", (req, res) => {
     return handle(req, res);
   });
 
-});
+  server.listen(port, err => {
+    if (err) throw err;
 
+    console.log(`Listening on port ${port}`);
+  });
+});
